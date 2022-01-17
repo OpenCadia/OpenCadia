af6ee28b61f5982c8c44e3c92e99b198be34c3f8
Jure Poljsak <jpoljsak@gmail.com>
2022-01-17 23:54:41 +0100

    commented some code that is not in use

--- obd/elm327.py
+++ obd/elm327.py
@@ -301,8 +301,8 @@ class ELM327:
         """
 
         # before we change the timout, save the "normal" value
-        timeout = self.__port.timeout
-        self.__port.timeout = self.timeout  # we're only talking with the ELM, so things should go quickly
+        #timeout = self.__port.timeout
+        #self.__port.timeout = self.timeout  # we're only talking with the ELM, so things should go quickly
 
         for baud in self._TRY_BAUDS:
             print ("Trying baud rate: ",baud)
@@ -331,12 +331,12 @@ class ELM327:
                 logger.debug("Choosing baud %d" % baud)
                 print("Response from baud %d: %s" % (baud, repr(response)))
                 print("Choosing baud rate %d" % baud)
-                self.__port.timeout = timeout  # reinstate our original timeout
+                #self.__port.timeout = timeout  # reinstate our original timeout
                 return True
 
         logger.debug("Failed to choose baud")
         print("Failed to choose baud rate")
-        self.__port.timeout = timeout  # reinstate our original timeout
+        #self.__port.timeout = timeout  # reinstate our original timeout
         return False
 
     def __isok(self, lines, expectEcho=False):
